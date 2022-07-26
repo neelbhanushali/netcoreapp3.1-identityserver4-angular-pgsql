b6f3422ea15bd7290f9a21e4346d968cdfafd78b
Neel Bhanushali <neal.bhanushali@gmail.com>
2022-07-26 15:50:39 +0530

    initial commit

---
+++ netcoreapp3.1 identityserver4 angular pgsql/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace netcoreapp3._1_identityserver4_angular_pgsql.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
---
+++ netcoreapp3.1 identityserver4 angular pgsql/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace netcoreapp3._1_identityserver4_angular_pgsql
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
